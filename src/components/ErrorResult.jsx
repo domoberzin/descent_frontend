8149237fbf3655152145e41d95d6f382e666b95a
dominic.chua <dominic.chua@kabam.ai>
2024-07-22 20:27:18 +0800

    Fix error component to allow html

--- src/components/ErrorResult.jsx
+++ src/components/ErrorResult.jsx
@@ -1,9 +1,13 @@
-// ErrorResult.jsx
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 function ErrorResult({ type, message, traceback }) {
+  // Function to replace <br> tags with actual line breaks
+  const formatTraceback = (text) => {
+    return text.replace(/<br>/g, '\n');
+  };
+
   return (
     <Card className="mb-3">
       <Card.Body>
@@ -14,7 +18,10 @@ function ErrorResult({ type, message, traceback }) {
           <ReactMarkdown>{`\`${message}\``}</ReactMarkdown>
         </Card.Text>
         <Card.Text>
-          <ReactMarkdown>{traceback}</ReactMarkdown>
+          <pre 
+            style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}
+            dangerouslySetInnerHTML={{ __html: formatTraceback(traceback) }} 
+          />
         </Card.Text>
       </Card.Body>
     </Card>
