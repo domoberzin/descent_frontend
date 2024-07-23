9a348faa882ec2ba6a82af19ba1cd2b61da44217
yhanyi <yeohhanyi0916@gmail.com>
2024-07-23 08:43:25 +0800

    Added more frontend stuff

---
+++ src/components/navbar/NavLinks.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "./NavLink";
+
+export const NavLinks = () => {
+  return (
+    <ul className="flex gap-3 text-zinc-400 md:gap-9">
+      <li>
+        <NavLink href="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink href="/problems">Problems</NavLink>
+      </li>
+      <li>
+        <NavLink href="/submission">Submit</NavLink>
+      </li>
+      <li>
+        <NavLink href="/contact">Contact</NavLink>
+      </li>
+    </ul>
+  );
+};
